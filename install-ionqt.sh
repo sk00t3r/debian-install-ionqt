46d3273af80eaac0702942f2e6182aa44cb3ac50
sk00t3r <johnsone@roadrunner.com>
2017-02-05 10:32:45 -0500

    Update install-ionqt.sh

--- install-ionqt.sh
+++ install-ionqt.sh
@@ -11,43 +11,43 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 clear
 echo "####Installing Dependencies####"
-sudo apt-get intsall autoconf
-sudo apt-get intsall autogen
-sudo apt-get intsall automake
-sudo apt-get intsall autotools-dev
-sudo apt-get intsall bsdmainutils
-sudo apt-get intsall build-essential
-sudo apt-get intsall git
-sudo apt-get intsall libboost-all-dev
-sudo apt-get intsall libboost-dev
-sudo apt-get intsall libboost-filesystem-dev
-sudo apt-get intsall libboost-program-options-dev
-sudo apt-get intsall libboost-system-dev
-sudo apt-get intsall libboost-thread-dev
-sudo apt-get intsall libcrypto++-dev
-sudo apt-get intsall libdb++-dev
-sudo apt-get intsall libdb5.3++-dev
-sudo apt-get intsall libdb5.3-dev
-sudo apt-get intsall libevent-dev
-sudo apt-get intsall libgmp3-dev
-sudo apt-get intsall libgmp-dev
-sudo apt-get intsall libminiupnpc-dev --with-miniupnpc and --enable-upnp-default
-sudo apt-get intsall libprotobuf-dev
-sudo apt-get intsall libqrencode-dev
-sudo apt-get intsall libqrencode-devinstall
-sudo apt-get intsall libqt5core5a
-sudo apt-get intsall libqt5dbus5
-sudo apt-get intsall libqt5gui5
-sudo apt-get intsall libssl-dev
-sudo apt-get intsall libtool
-sudo apt-get intsall make
-sudo apt-get intsall pkg-config
-sudo apt-get intsall protobuf-compiler
-sudo apt-get intsall qt5-default
-sudo apt-get intsall qt5-qmake
-sudo apt-get intsall qtbase5-dev-tools
-sudo apt-get intsall qttools5-dev
-sudo apt-get intsall qttools5-dev-tools
+sudo apt-get install autoconf
+sudo apt-get install autogen
+sudo apt-get install automake
+sudo apt-get install autotools-dev
+sudo apt-get install bsdmainutils
+sudo apt-get install build-essential
+sudo apt-get install git
+sudo apt-get install libboost-all-dev
+sudo apt-get install libboost-dev
+sudo apt-get install libboost-filesystem-dev
+sudo apt-get install libboost-program-options-dev
+sudo apt-get install libboost-system-dev
+sudo apt-get install libboost-thread-dev
+sudo apt-get install libcrypto++-dev
+sudo apt-get install libdb++-dev
+sudo apt-get install libdb5.3++-dev
+sudo apt-get install libdb5.3-dev
+sudo apt-get install libevent-dev
+sudo apt-get install libgmp3-dev
+sudo apt-get install libgmp-dev
+sudo apt-get install libminiupnpc-dev --with-miniupnpc and --enable-upnp-default
+sudo apt-get install libprotobuf-dev
+sudo apt-get install libqrencode-dev
+sudo apt-get install libqrencode-devinstall
+sudo apt-get install libqt5core5a
+sudo apt-get install libqt5dbus5
+sudo apt-get install libqt5gui5
+sudo apt-get install libssl-dev
+sudo apt-get install libtool
+sudo apt-get install make
+sudo apt-get install pkg-config
+sudo apt-get install protobuf-compiler
+sudo apt-get install qt5-default
+sudo apt-get install qt5-qmake
+sudo apt-get install qtbase5-dev-tools
+sudo apt-get install qttools5-dev
+sudo apt-get install qttools5-dev-tools
 clear
 echo "####Cloning Repo####"
 git clone https://github.com/Ion-Network/Ion-Core
