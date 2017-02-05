7635feb3dcb022b208c8476e4f3991d08e6ba87a
sk00t3r <johnsone@roadrunner.com>
2017-02-05 00:33:13 -0500

    Update install-ionqt.sh

--- install-ionqt.sh
+++ install-ionqt.sh
@@ -8,7 +8,8 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 echo "Installing Dependencies"
-sudo apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libqrencode-dev libminiupnpc-dev (see --with-miniupnpc and --enable-upnp-default)
+sudo apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libqrencode-dev -y
+sudo apt-get install libminiupnpc-dev (see --with-miniupnpc and --enable-upnp-default) -y
 echo "Cloning Repo"
 sudo git clone https://github.com/Ion-Network/Ion-Core
 echo "Changing To The Ion-Core Directory"
