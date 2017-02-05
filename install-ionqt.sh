6788d1b4ca31a2069f13259b6f344df31d950eaa
sk00t3r <johnsone@roadrunner.com>
2017-02-05 01:40:13 -0500

    Update install-ionqt.sh

--- install-ionqt.sh
+++ install-ionqt.sh
@@ -11,20 +11,21 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 clear
 echo "####Installing Dependencies####"
-sudo apt-get install git build-essential automake libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libqrencode-dev -y
+sudo apt-get install git build-essential automake libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libqrencode-dev  libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler build-essential libtool autotools-dev autoconf pkg-config libssl-dev libgmp3-dev libevent-dev bsdmainutils -y
 sudo apt-get install libminiupnpc-dev --with-miniupnpc and --enable-upnp-default -y
 clear
 echo "####Cloning Repo####"
 sudo git clone https://github.com/Ion-Network/Ion-Core
-#clear
+clear
 echo "####Changing To The Ion-Core Directory####"
 cd Ion-Core/
-#clear
+clear
 echo "####Making####"
+sudo qmake
 sudo make
-#clear
+clear
 echo "####Installing####"
 sudo install
-#clear
+clear
 echo "####Starting Ion-QT Wallet####"
 sudo ./ionx-qt
