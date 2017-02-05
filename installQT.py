2e9f1264c4a1ec3c6e058c6400f063c331f3479d
sk00t3r <johnsone@roadrunner.com>
2017-02-05 18:07:46 -0500

    Update installQT.py

--- installQT.py
+++ installQT.py
@@ -9,12 +9,17 @@ distro = platform.linux_distribution()[0]
 version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
 
-if name == "xenial" or "yakkety":
+if version == "16.04":
         urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh", "ubuntu16.sh")
         os.chmod('ubuntu16.sh', 0755)        
         subprocess.call("./ubuntu16.sh", shell=True)
 
-elif name == "trusty":
+elif version == "16.10":
+        urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh", "ubuntu16.sh")
+        os.chmod('ubuntu16.sh', 0755)        
+        subprocess.call("./ubuntu16.sh", shell=True)
+
+elif version == "14.04":
         urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu14.sh", "ubuntu14.sh")
         os.chmod('ubuntu14.sh', 0755)
         subprocess.call("./ubuntu14.sh", shell=True)
