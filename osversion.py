b967a9be085bc390a23749b46c95b1a23ea0001d
sk00t3r <johnsone@roadrunner.com>
2017-02-05 11:53:55 -0500

    Update osversion.py

--- osversion.py
+++ osversion.py
@@ -7,6 +7,6 @@ version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
 
 if version == 16.04 or version == 16.10
-  subprocess.call("16.xx.sh", shell=True)
+  subprocess.call("Ubuntu-16.xx-Test.sh")
  else 
   print "OS not Supported Yet."
