8c9fae2248db4fb186aee61c746199ffc00c2c29
sk00t3r <johnsone@roadrunner.com>
2017-02-05 15:47:25 -0500

    Update installQT.py

--- installQT.py
+++ installQT.py
@@ -8,9 +8,11 @@ version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
 
 if name == "Xenial Xerus" or "Yakkety Yak":
+        sudo wget https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh
         subprocess.call("./ubuntu16.sh", shell=True)
 
 elif name == "Trusty Tahr":
+        sudo wget https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu14.sh
         subprocess.call("./ubuntu14.sh", shell=True)
 
 else:
