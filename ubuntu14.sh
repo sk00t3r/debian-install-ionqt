895b394b0707112a13d2dd3c5b675b5e996b147a
sk00t3r <johnsone@roadrunner.com>
2017-02-05 18:23:24 -0500

    Update ubuntu14.sh

--- ubuntu14.sh
+++ ubuntu14.sh
@@ -13,8 +13,8 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 clear
-echo "#### Installing Dependencie s####"
-sudo apt-get install autoconf autogen automake autotools-dev bsdmainutils build-essential git libboost-all-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libcrypto++-dev libdb++-dev libdb4.8-dev libdb4.8++-dev libevent-dev libgmp3-dev libgmp-dev libprotobuf-dev libqrencode-dev libqrencode-devinstall libqt5core5a libqt5dbus5 libqt5gui5 libssl-dev libtool make pkg-config protobuf-compiler qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev qttools5-dev-tools -y
+echo "#### Installing Dependencies ####"
+sudo apt-get install autoconf autogen automake autotools-dev bsdmainutils build-essential git libboost-all-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libcrypto++-dev libdb++-dev libdb4.8-dev libdb4.8++-dev libevent-dev libgmp3-dev libgmp-dev libprotobuf-dev libqrencode-dev libqrencode-devinstall libqt4-dev libqt5core5a libqt5dbus5 libqt5gui5 libssl-dev libtool make pkg-config protobuf-compiler qt4-qmake qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev qttools5-dev-tools -y
 sudo apt-get install libminiupnpc-dev --with-miniupnpc and --enable-upnp-default -y 
 clear
 echo "#### Cloning Repo ####"
