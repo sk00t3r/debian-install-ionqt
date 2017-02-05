91448cb56d1866dc1b561c347b2e56274466778e
sk00t3r <johnsone@roadrunner.com>
2017-02-05 11:51:59 -0500

    Update osversion.py

--- osversion.py
+++ osversion.py
@@ -5,3 +5,8 @@ import platform
 distro = platform.linux_distribution()[0]
 version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
+
+if version == 16.04 or version == 16.10
+  subprocess.call("16.xx.sh", shell=True)
+ else 
+  print "OS not Supported Yet."
