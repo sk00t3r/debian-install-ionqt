e478c88ac8506677ab8e47fccd44d9edd6966016
sk00t3r <johnsone@roadrunner.com>
2017-02-05 11:04:45 -0500

    Update install-ionqt.sh

--- install-ionqt.sh
+++ install-ionqt.sh
@@ -11,56 +11,17 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 clear
 echo "####Installing Dependencies####"
-sudo apt-get install autoconf
-sudo apt-get install autogen
-sudo apt-get install automake
-sudo apt-get install autotools-dev
-sudo apt-get install bsdmainutils
-sudo apt-get install build-essential
-sudo apt-get install git
-sudo apt-get install libboost-all-dev
-sudo apt-get install libboost-dev
-sudo apt-get install libboost-filesystem-dev
-sudo apt-get install libboost-program-options-dev
-sudo apt-get install libboost-system-dev
-sudo apt-get install libboost-thread-dev
-sudo apt-get install libcrypto++-dev
-sudo apt-get install libdb++-dev
-sudo apt-get install libdb5.3++-dev
-sudo apt-get install libdb5.3-dev
-sudo apt-get install libevent-dev
-sudo apt-get install libgmp3-dev
-sudo apt-get install libgmp-dev
-sudo apt-get install libminiupnpc-dev --with-miniupnpc and --enable-upnp-default
-sudo apt-get install libprotobuf-dev
-sudo apt-get install libqrencode-dev
-sudo apt-get install libqrencode-devinstall
-sudo apt-get install libqt5core5a
-sudo apt-get install libqt5dbus5
-sudo apt-get install libqt5gui5
-sudo apt-get install libssl-dev
-sudo apt-get install libtool
-sudo apt-get install make
-sudo apt-get install pkg-config
-sudo apt-get install protobuf-compiler
-sudo apt-get install qt5-default
-sudo apt-get install qt5-qmake
-sudo apt-get install qtbase5-dev-tools
-sudo apt-get install qttools5-dev
-sudo apt-get install qttools5-dev-tools
+sudo apt-get install autoconf autogen automake autotools-dev bsdmainutils build-essential git libboost-all-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libcrypto++-dev libdb++-dev libdb5.3++-dev libdb5.3-dev libevent-dev libgmp3-dev libgmp-dev libminiupnpc-dev --with-miniupnpc and --enable-upnp-default libprotobuf-dev libqrencode-dev libqrencode-devinstall libqt5core5a libqt5dbus5 libqt5gui5 libssl-dev libtool make pkg-config protobuf-compiler qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev qttools5-dev-tools -y
 clear
 echo "####Cloning Repo####"
 git clone https://github.com/Ion-Network/Ion-Core
 clear
 echo "####Changing To The Ion-Core Directory####"
 cd Ion-Core/
-#clear
+clear
 echo "####Making####"
 sudo qmake
 sudo make
-#clear
-echo "####Installing####"
-sudo install
-#clear
+clear
 echo "####Starting Ion-QT Wallet####"
 sudo ./ionx-qt
