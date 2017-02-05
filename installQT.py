fc751257f219b6d2df1960e84764f7bfafd48b14
sk00t3r <johnsone@roadrunner.com>
2017-02-05 15:59:55 -0500

    Update installQT.py

--- installQT.py
+++ installQT.py
@@ -2,13 +2,14 @@
 
 import platform
 import subprocess
+import wget
 
 distro = platform.linux_distribution()[0]
 version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
 
 if name == "Xenial Xerus" or "Yakkety Yak":
-        sudo wget https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh
+        url = 'https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh'
         sudo chmod 755 ubuntu16.sh        
         subprocess.call("./ubuntu16.sh", shell=True)
 
