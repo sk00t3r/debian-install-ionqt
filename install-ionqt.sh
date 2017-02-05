edae296ed78839bd33001562c98224a14c2b263d
sk00t3r <johnsone@roadrunner.com>
2017-02-05 00:36:12 -0500

    Update install-ionqt.sh

--- install-ionqt.sh
+++ install-ionqt.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-echo "Changing to home directory"
+echo "####Changing to home directory####"
 cd ~
-echo "Installing Sudo"
+clear
+echo "####Installing Sudo####"
 apt-get install sudo -y
-echo "Updating Ubuntu/Debian"
+clear
+echo "####Updating Ubuntu/Debian####"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-echo "Installing Dependencies"
+clear
+echo "####Installing Dependencies####"
 sudo apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libqrencode-dev -y
 sudo apt-get install libminiupnpc-dev (see --with-miniupnpc and --enable-upnp-default) -y
-echo "Cloning Repo"
+clear
+echo "####Cloning Repo####"
 sudo git clone https://github.com/Ion-Network/Ion-Core
-echo "Changing To The Ion-Core Directory"
+clear
+echo "####Changing To The Ion-Core Directory####"
 cd Ion-Core/
-echo "Making"
+clear
+echo "####Making####"
 sudo make
-echo "Installing"
+clear
+echo "####Installing####"
 sudo install
-echo "Starting Ion-QT Wallet"
+clear
+echo "####Starting Ion-QT Wallet####"
 sudo ./ionx-qt
