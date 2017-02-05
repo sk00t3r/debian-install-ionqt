4848cf90abfda1aaf3ad5efd187028e0d750a43a
sk00t3r <johnsone@roadrunner.com>
2017-02-05 00:21:11 -0500

    Update version16.sh

--- version16.sh
+++ version16.sh
@@ -8,12 +8,14 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 echo "Installing Dependencies"
-sudo apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev automake libminiupnpc-dev libgmp-dev libboost-all-dev git build-essential libssl-dev libboost-all-dev libdb5.3-dev git libdb5.3++-dev libminiupnpc-dev make zip automake autogen libtool qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libdb++-dev libminiupnpc-dev libqrencode-devinstall libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libqrencode-dev libminiupnpc-dev (see --with-miniupnpc and --enable-upnp-default)
 echo "Cloning Repo"
 sudo git clone https://github.com/Ion-Network/Ion-Core
 echo "Changing To The Ion-Core Directory"
 cd Ion-Core/
 echo "Making"
 sudo make
+echo "Installing"
+sudo install
 echo "Starting Ion-QT Wallet"
 sudo ./ionx-qt
