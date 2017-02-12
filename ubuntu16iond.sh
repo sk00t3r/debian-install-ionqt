5dc2ab102a235a9e6eece7b73156e16655dd2d63
sk00t3r <johnsone@roadrunner.com>
2017-02-12 17:07:20 -0500

    Update ubuntu16iond.sh

--- ubuntu16iond.sh
+++ ubuntu16iond.sh
@@ -4,11 +4,11 @@ echo "#### Installing Sudo ####"
 apt-get install sudo -y
 clear
 sudo "#### Stopping iond ####"
-iond stop
+sudo iond stop
 echo "### Removing old ION"
-rm -rf /usr/bin/iond
-rm -rf /usr/local/bin/iond
-rm -rf /root/ion
+sudo rm -rf /usr/bin/iond
+sudo rm -rf /usr/local/bin/iond
+sudo rm -rf /root/ion
 clear
 echo "#### Change to home directory ####"
 cd ~/
@@ -43,8 +43,6 @@ sudo apt-get install autoconf -y
 sudo apt-get install pkg-config -y
 sudo apt-get install libssl-dev -y
 sudo apt-get install libcrypto++-dev -4
-sudo apt-get install -y
-sudo apt-get install -y
 sudo apt-get install libdb5.3++-dev -y
 clear
 # echo "#### Installing silknetwork repository ####"
@@ -57,16 +55,16 @@ git clone https://github.com/ionomy/ion
 clear
 echo "#### Installing IOND Core ####"
 cd ion/src
-make -f makefile.unix
-mv ~/ion/src/xiond ~/ion/src/iond
-chmod 755 ~/ion/src/iond
-mv ~/ion/src/iond /usr/bin
+sudo make -f makefile.unix
+sudo mv ~/ion/src/xiond ~/ion/src/iond
+sudo chmod 755 ~/ion/src/iond
+sudo mv ~/ion/src/iond /usr/bin
 clear
 echo "#### Creating ION folder ####"
-mkdir ~/.ionomy/
+sudo mkdir ~/.ionomy/
 clear
 echo "#### Backing up & Moving Old ION Wallet.dat & ion.conf ####"
-cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
-cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
-mv ~/.ion/wallet.dat ~/.ionomy/
-mv ~/.ion/ion.conf ~/.ionomy/
+sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
+sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
+sudo mv ~/.ion/wallet.dat ~/.ionomy/
+sudo mv ~/.ion/ion.conf ~/.ionomy/
