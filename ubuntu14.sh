2aaa12cddde64542a23bb8520b2a1d8c11fe7d52
sk00t3r <johnsone@roadrunner.com>
2017-02-12 21:03:56 -0500

    Update ubuntu14.sh

--- ubuntu14.sh
+++ ubuntu14.sh
@@ -59,7 +59,7 @@ echo "#### Cloning Repo ####"
 git clone https://github.com/ionomy/ion
 clear
 echo "#### Changing To The Ion-Core Directory ####"
-cd Ion-Core/
+cd ion/
 clear
 echo "#### Making ####"
 sudo qmake
