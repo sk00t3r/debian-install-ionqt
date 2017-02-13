4f29fb651ed5655fc5b85409d812bce845a2512c
sk00t3r <johnsone@roadrunner.com>
2017-02-13 01:40:57 -0500

    Update ubuntu16.sh

--- ubuntu16.sh
+++ ubuntu16.sh
@@ -63,28 +63,6 @@ echo "#### Changing To The Ion-Core Directory ####"
 echo " "
 cd ion/
 clear
-read -r -p "Would you like your wallet.dat and ion.conf file backedup and auto-restored? Type no if this is is a new install [Y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-  then
-  echo "Great we will begin backing up your files and noving them to new wallet"
-  echo " "
-  echo "#### Creating ION folder ####"
-  echo " "
-  sudo mkdir ~/.ionomy/
-  echo "#### Backing up & Moving Old ION wallet.dat & ion.conf ####"
-  echo " "
-  sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
-  sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
-  sudo mv ~/.ion/wallet.dat ~/.ionomy/
-  sudo mv ~/.ion/ion.conf ~/.ionomy/
-  echo "#### Making ####"
-  echo " "
-  sudo qmake
-  sudo make
-  echo "#### Starting Ion-QT Wallet ####"
-  echo " "
-  sudo ./ionx-qt
-else 
 echo " Okay, we will not backup your files."
 echo " "
 echo "#### Making ####"
@@ -94,4 +72,3 @@ sudo make
 echo "#### Starting Ion-QT Wallet ####"
 echo " "
 sudo ./ionx-qt
-fi
