03e2da503e02ae68ca3c4e213e0e824f8b6ef40e
sk00t3r <johnsone@roadrunner.com>
2017-02-13 17:49:32 -0500

    Update installQTvps.py

--- installQTvps.py
+++ installQTvps.py
@@ -19,7 +19,7 @@ elif version == "16.10":
         os.chmod('ubuntu16vps.sh', 0755)        
         subprocess.call("./ubuntu16vps.sh", shell=True)
         
-elif distro == "debian":
+elif version == "8.0":
         urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16vps.sh", "ubuntu16vps.sh")
         os.chmod('ubuntu16vps.sh', 0755)        
         subprocess.call("./ubuntu16vps.sh", shell=True)
