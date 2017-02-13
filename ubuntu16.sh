44174b88a32af7258764b001faa5021551a4be4f
sk00t3r <johnsone@roadrunner.com>
2017-02-12 23:18:23 -0500

    Update ubuntu16.sh

--- ubuntu16.sh
+++ ubuntu16.sh
@@ -4,15 +4,18 @@ echo "#### Changing to home directory ####"
 cd ~
 clear
 echo "#### Installing Sudo ####"
+echo " "
 apt-get install sudo -y
 clear
 echo "#### Updating Ubuntu/Debian ####"
+echo " "
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get update -y
 clear
 echo "#### Installing Dependencies ####"
+echo " "
 sudo apt-get install autoconf -y 
 sudo apt-get install autogen -y
 sudo apt-get install automake -y
@@ -53,32 +56,42 @@ sudo apt-get install qttools5-dev-tools -y
 sudo apt-get install libminiupnpc-dev -y
 clear
 echo "#### Cloning Repo ####"
+echo " "
 git clone https://github.com/ionomy/ion
 clear
 echo "#### Changing To The Ion-Core Directory ####"
+echo " "
 cd ion/
 clear
 read -r -p "Would you like your wallet.dat and ion.conf file backedup and auto-restored? Type no if this is is a new install [Y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
   then
   echo "Great we will begin backing up your files and noving them to new wallet"
+  echo " "
   echo "#### Creating ION folder ####"
+  echo " "
   sudo mkdir ~/.ionomy/
   echo "#### Backing up & Moving Old ION wallet.dat & ion.conf ####"
+  echo " "
   sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
   sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
   sudo mv ~/.ion/wallet.dat ~/.ionomy/
   sudo mv ~/.ion/ion.conf ~/.ionomy/
   echo "#### Making ####"
+  echo " "
   sudo qmake
   sudo make
   echo "#### Starting Ion-QT Wallet ####"
+  echo " "
   sudo ./ionx-qt
 else 
 echo " Okay, we will not backup your files."
-#echo "#### Making ####"
+echo " "
+echo "#### Making ####"
+echo " "
 sudo qmake
 sudo make
 echo "#### Starting Ion-QT Wallet ####"
+echo " "
 sudo ./ionx-qt
 fi
