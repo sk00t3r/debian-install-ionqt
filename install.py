cb809b1a6d436440fd7877d682c79ebfae86407c
sk00t3r <johnsone@roadrunner.com>
2017-02-18 01:01:34 -0500

    Update install.py

--- install.py
+++ install.py
@@ -12,17 +12,17 @@ name =  platform.linux_distribution()[2]
 
 def qt():
         if version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh", "ubuntu16.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/qt/ubuntu16.sh", "ubuntu16.sh")
                 os.chmod('ubuntu16.sh', 0755)
                 subprocess.call("./ubuntu16.sh", shell=True)
 
         elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16.sh", "ubuntu16.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/qt/ubuntu16.sh", "ubuntu16.sh")
                 os.chmod('ubuntu16.sh', 0755)
                 subprocess.call("./ubuntu16.sh", shell=True)
 
         elif version == "14.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu14.sh", "ubuntu14.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/qt/ubuntu14.sh", "ubuntu14.sh")
                 os.chmod('ubuntu14.sh', 0755)
                 subprocess.call("./ubuntu14.sh", shell=True)
         else:
@@ -30,15 +30,15 @@ def qt():
 
 def vps():
         if version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16vps.sh", "ubuntu16vps.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/qt/untu16vps.sh", "ubuntu16vps.sh")
                 os.chmod('ubuntu16vps.sh', 0755)
                 subprocess.call("./ubuntu16vps.sh", shell=True)
         elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu16vps.sh", "ubuntu16vps.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/qt/ubuntu16vps.sh", "ubuntu16vps.sh")
                 os.chmod('ubuntu16vps.sh', 0755)
                 subprocess.call("./ubuntu16vps.sh", shell=True)
         elif version == "14.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/ubuntu14vps.sh", "ubuntu14vps.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/qt/ubuntu14vps.sh", "ubuntu14vps.sh")
                 os.chmod('ubuntu14vps.sh', 0755)
                 subprocess.call("./ubuntu14vps.sh", shell=True)
         else:
@@ -46,16 +46,27 @@ def vps():
 
 def arm():
         if version == "8.0":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/raspberry_pi/rpi.sh", "rpi.sh")
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/raspberry_pi/rpi.sh", "rpi.sh")
                 os.chmod('rpi.sh', 0755)
                 subprocess.call("./rpi.sh", shell=True)
         else:
                 print("Sorry version not yet supported.")
 
 def iond():
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/debian-install-ionqt/master/raspberry_pi/rpi.sh", "rpi.sh")
-                os.chmod('rpi.sh', 0755)
-                subprocess.call("./rpi.sh", shell=True)
+        if version == "16.04":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
+                os.chmod('ubuntu16iond.sh', 0755)
+                subprocess.call("./ubuntu16iond.sh", shell=True)
+        elif version == "16.10":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
+                os.chmod('ubuntu16iond.sh', 0755)
+                subprocess.call("./ubuntu16iond.sh", shell=True)
+        elif version == "14.04":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/testing/debian/iond/ubuntu14iond.sh", "ubuntu14iond.sh")
+                os.chmod('ubuntu14iond.sh', 0755)
+                subprocess.call("./ubuntu14iond.sh", shell=True)
+        else:
+                print("Sorry version not yet supported.")
 
 choice = raw_input(" Type 'A' to install the QT wallet on your personal dekstop or laptop. \n Tpye 'B' to install the QT wallet on a low RAM VPS. \n Type 'C' to install the QT wallet on a Raspberry Pi or a Pine64. \n Type 'D' to install the ion.d service only. \n \n ")
 
