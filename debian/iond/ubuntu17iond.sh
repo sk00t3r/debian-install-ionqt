6ef195e6653da77b797dcb1437c052186121e861
sk00t3r <johnsone@roadrunner.com>
2017-02-26 07:39:46 -0500

    Update ubuntu17iond.sh

--- debian/iond/ubuntu17iond.sh
+++ debian/iond/ubuntu17iond.sh
@@ -13,12 +13,12 @@ clear
 echo "#### Change to home directory ####"
 cd ~/
 clear
-echo "#### Creating Swap ####"
-sudo fallocate -l 4G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo swapon -s
+#echo "#### Creating Swap ####"
+#sudo fallocate -l 4G /swapfile
+#sudo chmod 600 /swapfile
+#sudo mkswap /swapfile
+#sudo swapon /swapfile
+#sudo swapon -s
 sudo echo "/swapfile swap sw 0 0" >> /etc/fstab
 clear
 echo "#### Updating Debian 14.xx ####"
