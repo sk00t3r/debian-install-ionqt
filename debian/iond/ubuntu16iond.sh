31a11174a9a2048a5ede1d0e4063c696303068de
sk00t3r <johnsone@roadrunner.com>
2017-02-26 07:42:57 -0500

    Update ubuntu16iond.sh

--- debian/iond/ubuntu16iond.sh
+++ debian/iond/ubuntu16iond.sh
@@ -21,7 +21,7 @@ clear
 #sudo swapon -s
 #sudo echo "/swapfile swap sw 0 0" >> /etc/fstab
 c#lear
-echo "#### Updating Debian 14.xx ####"
+echo "#### Updating Debian 16.xx ####"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
