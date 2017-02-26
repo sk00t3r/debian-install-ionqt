0e6b01fc79bead5c7cd0fb9c4c74294131edbedc
sk00t3r <johnsone@roadrunner.com>
2017-02-26 15:17:31 -0500

    Update ubuntu14vps_iond.sh

--- debian/vps_iond/ubuntu14vps_iond.sh
+++ debian/vps_iond/ubuntu14vps_iond.sh
@@ -1,1 +1,13 @@
 test
+
+
+
+
+echo "#### Creating Swap ####"
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon -s
+sudo echo "/swapfile swap sw 0 0" >> /etc/fstab
+clear
