05a753d63c98ee999234b27ff0cf80e633fef96e
sk00t3r <johnsone@roadrunner.com>
2017-02-26 13:16:07 -0500

    Update ubuntu14iond.sh

--- debian/iond/ubuntu14iond.sh
+++ debian/iond/ubuntu14iond.sh
@@ -13,14 +13,6 @@ clear
 echo "#### Change to home directory ####"
 cd ~/
 clear
-#echo "#### Creating Swap ####"
-#sudo fallocate -l 4G /swapfile
-#sudo chmod 600 /swapfile
-#sudo mkswap /swapfile
-#sudo swapon /swapfile
-#sudo swapon -s
-#sudo echo "/swapfile swap sw 0 0" >> /etc/fstab
-#clear
 echo "#### Updating Debian 14.xx ####"
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -64,8 +56,32 @@ clear
 echo "#### Creating ION folder ####"
 sudo mkdir ~/.ionomy/
 clear
-echo "#### Backing up & Moving Old ION Wallet.dat & ion.conf ####"
-sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
-sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
-sudo mv ~/.ion/wallet.dat ~/.ionomy/
-sudo mv ~/.ion/ion.conf ~/.ionomy/
+echo "#### Chevking for existing iond install ####"
+echo " "
+if [ -n "$(ls -A ~/.ionomy/wallet.dat)" ] && [ -n "$(ls -A ~/.ionomy/ion.conf)" ]
+then
+  clear
+  echo "#### Backing up original ion wallet.dat & ion.conf ####"
+  echo " "
+  sudo cp -p -f -r ~/.ionomy/wallet.dat ~/.ionomy/wallet.dat.backup
+  sudo cp -p -f -r ~/.ionomy/ion.conf ~/.ionomy/ion.conf.backup
+elif [ -n "$(ls -A ~/.ion/wallet.dat)" ] && [ -n "$(ls -A ~/.ion/ion.conf)" ]
+  then
+  clear
+  echo "#### Backing up & moving old ion wallet.dat & ion.conf ####"
+  echo " "
+  sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
+  sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
+  sudo mv ~/.ion/wallet.dat ~/.ionomy/
+  sudo mv ~/.ion/ion.conf ~/.ionomy/
+else
+  clear
+  echo "#### Please set a username and password, the password should be long and random ####"
+  echo "#### Ctrl + X, Y, Enter to save file and exit ####"
+  echo " "
+  read -p "#### Press any key when you are ready to continue ####"
+  Echo " "
+  sudo wget https://raw.githubusercontent.com/sk00t3r/linux-ion/master/ion.conf
+  sudo nano ion.conf
+fi
+clear
