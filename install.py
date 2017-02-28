330541462b23d48bb40296d8767533dcb4406e4f
sk00t3r <johnsone@roadrunner.com>
2017-02-27 23:11:30 -0500

    Update install.py

--- install.py
+++ install.py
@@ -10,61 +10,48 @@ version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
 
 def qt():
-        if version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16.sh", "ubuntu16.sh")
-                os.chmod('ubuntu16.sh', 0755)
-                subprocess.call("./ubuntu16.sh", shell=True)
+        if version == "14.04" and ram > 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu14qt.sh", "ubuntu14qt.sh")
+                os.chmod('ubuntu14qt.sh', 0755)
+                subprocess.call("./ubuntu14qt.sh", shell=True)
+                
+        elif version == "14.04":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu14qt_lowram.sh", "ubuntu14qt_lowram.sh")
+                os.chmod('ubuntu14qt_lowram.sh', 0755)
+                subprocess.call("./ubuntu14qt_lowram.sh", shell=Tru
+        
+        elif version == "16.04" and ram > 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16qt.sh", "ubuntu16qt.sh")
+                os.chmod('ubuntu16qt.sh', 0755)
+                subprocess.call("./ubuntu16qt.sh", shell=True)
 
         elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16.sh", "ubuntu16.sh")
-                os.chmod('ubuntu16.sh', 0755)
-                subprocess.call("./ubuntu16.sh", shell=True)
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16qt_lowram.sh", "ubuntu16qt_lowram.sh")
+                os.chmod('ubuntu16qt_lowram.sh', 0755)
+                subprocess.call("./ubuntu16qt_lowram.sh", shell=True)
+                
+        elif version == "18.1" and mem > 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16qt.sh", "ubuntu16qt.sh")
+                os.chmod('ubuntu16qt.sh', 0755)
+                subprocess.call("./ubuntu16qt.sh", shell=True)
                 
         elif version == "18.1":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16.sh", "ubuntu16.sh")
-                os.chmod('ubuntu16.sh', 0755)
-                subprocess.call("./ubuntu16.sh", shell=True)
-
-        elif version == "14.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu14.sh", "ubuntu14.sh")
-                os.chmod('ubuntu14.sh', 0755)
-                subprocess.call("./ubuntu14.sh", shell=True)
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16qt_lowram.sh", "ubuntu16qt_lowram.sh")
+                os.chmod('ubuntu16qt_lowram.sh', 0755)
+                subprocess.call("./ubuntu16qt_lowram.sh", shell=True)
                 
-        elif version == "17.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu17.sh", "ubuntu17.sh")
-                os.chmod('ubuntu17.sh', 0755)
-                subprocess.call("./ubuntu17.sh", shell=True)
+        elif version == "17.04" and mem > 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu17qt.sh", "ubuntu17qt.sh")
+                os.chmod('ubuntu17qt.sh', 0755)
+                subprocess.call("./ubuntu17qt.sh", shell=True)
+                                
+         elif version == "17.04":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu17qt_lowram.sh", "ubuntu17qt_lowram.sh")
+                os.chmod('ubuntu17qt_lowram.sh', 0755)
+                subprocess.call("./ubuntu17qt_lowram.sh", shell=True)
         else:
                 print("Sorry version not yet supported.")
-
-def vps_qt():
-        if version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/untu16vps.sh", "ubuntu16vps.sh")
-                os.chmod('ubuntu16vps.sh', 0755)
-                subprocess.call("./ubuntu16vps.sh", shell=True)
-        
-        elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16vps.sh", "ubuntu16vps.sh")
-                os.chmod('ubuntu16vps.sh', 0755)
-                subprocess.call("./ubuntu16vps.sh", shell=True)
-        
-        elif version == "18.1":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu16vps.sh", "ubuntu16vps.sh")
-                os.chmod('ubuntu16vps.sh', 0755)
-                subprocess.call("./ubuntu16vps.sh", shell=True)
-        
-        elif version == "14.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu14vps.sh", "ubuntu14vps.sh")
-                os.chmod('ubuntu14vps.sh', 0755)
-                subprocess.call("./ubuntu14vps.sh", shell=True)
-                
-        elif version == "17.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/qt/ubuntu17vps.sh", "ubuntu17vps.sh")
-                os.chmod('ubuntu17vps.sh', 0755)
-                subprocess.call("./ubuntu17vps.sh", shell=True)
-        else:
-                print("Sorry version not yet supported.")
-
+                                
 def arm_qt():
         if version == "8.0":
                 urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/raspberry_pi/rpi_qt.sh", "rpi_qt.sh")
@@ -82,80 +69,61 @@ def arm_iond():
                 print("Sorry version not yet supported.")
                 
 def iond():
-        if version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
-                os.chmod('ubuntu16iond.sh', 0755)
-                subprocess.call("./ubuntu16iond.sh", shell=True)
-        
-        elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
-                os.chmod('ubuntu16iond.sh', 0755)
-                subprocess.call("./ubuntu16iond.sh", shell=True)
-        
-        elif version == "18.1":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
-                os.chmod('ubuntu16iond.sh', 0755)
-                subprocess.call("./ubuntu16iond.sh", shell=True)
-        
-        elif version == "14.04":
+        if version == "14.04" and ram > 4:
                 urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu14iond.sh", "ubuntu14iond.sh")
                 os.chmod('ubuntu14iond.sh', 0755)
                 subprocess.call("./ubuntu14iond.sh", shell=True)
                 
-        elif version == "17.04":
+        elif version == "14.04":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu14iond_lowram.sh", "ubuntu14iond_lowram.sh")
+                os.chmod('ubuntu14iond_lowram.sh', 0755)
+                subprocess.call("./ubuntu14iond_lowram.sh", shell=Tru
+        
+        elif version == "16.04" and ram > 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
+                os.chmod('ubuntu16iond.sh', 0755)
+                subprocess.call("./ubuntu16iond.sh", shell=True)
+
+        elif version == "16.10":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond_lowram.sh", "ubuntu16iond_lowram.sh")
+                os.chmod('ubuntu16iond_lowram.sh', 0755)
+                subprocess.call("./ubuntu16iond_lowram.sh", shell=True)
+                
+        elif version == "18.1" and mem > 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond.sh", "ubuntu16iond.sh")
+                os.chmod('ubuntu16iond.sh', 0755)
+                subprocess.call("./ubuntu16iond.sh", shell=True)
+                
+        elif version == "18.1":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu16iond_lowram.sh", "ubuntu16iond_lowram.sh")
+                os.chmod('ubuntu16iod_lowram.sh', 0755)
+                subprocess.call("./ubuntu16iond_lowram.sh", shell=True)
+                
+        elif version == "17.04" and mem > 4:
                 urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu17iond.sh", "ubuntu17iond.sh")
                 os.chmod('ubuntu17iond.sh', 0755)
                 subprocess.call("./ubuntu17iond.sh", shell=True)
+                                
+         elif version == "17.04":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/iond/ubuntu17iond_lowram.sh", "ubuntu17iond_lowram.sh")
+                os.chmod('ubuntu17iond_lowram.sh', 0755)
+                subprocess.call("./ubuntu17iond_lowram.sh", shell=True)
         else:
                 print("Sorry version not yet supported.")
                 
-def vps_iond():
-        if version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/vps_iond/ubuntu16vps_iond.sh", "ubuntu16vps_iond.sh")
-                os.chmod('ubuntu16vps_iond.sh', 0755)
-                subprocess.call("./ubuntu16vps_iond.sh", shell=True)
-        
-        elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/vps_iond/ubuntu16vps_iond.sh", "ubuntu16vps_iond.sh")
-                os.chmod('ubuntu16vps_iond.sh', 0755)
-                subprocess.call("./ubuntu16vps_iond.sh", shell=True)
-        
-        elif version == "18.1":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/vps_iond/ubuntu16ivps_ond.sh", "ubuntu16vps_iond.sh")
-                os.chmod('ubuntu16iond.sh', 0755)
-                subprocess.call("./ubuntu16vps_iond.sh", shell=True)
-        
-        elif version == "14.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/vps_iond/ubuntu14vps_iond.sh", "ubuntu14vps_iond.sh")
-                os.chmod('ubuntu14vps_iond.sh', 0755)
-                subprocess.call("./ubuntu14vps_iond.sh", shell=True)
-                
-        elif version == "17.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/linux-ion/master/debian/vps_iond/ubuntu17vps_iond.sh", "ubuntu17vps_iond.sh")
-                os.chmod('ubuntu17vps_iond.sh', 0755)
-                subprocess.call("./ubuntu17vps_iond.sh", shell=True)
-        else:
-                print("Sorry version not yet supported.")
-
-choice = raw_input(" \n Type 'A' to install the QT wallet on your personal dekstop or laptop. \n \n Tpye 'B' to install the QT wallet on a low RAM VPS. \n \n Type 'C' to install the QT wallet on a Raspberry Pi or a Pine64. \n \n Type 'D' to install the ion.d service on your personal desktop or laptop. \n \n Type 'E' to install the ion.d service on a low RAM VPS. \n \n Type 'F' to install the ion.d service on a Raspberry Pi or a Pine64. \n \n \n Type 'Q' to quit. \n \n \n ")
+choice = raw_input(" \n Type 'A' to install the ion QT wallet. \n \n Type 'B' to install the ion.d service. \n \n Type 'C' to install the QT wallet on your ARM devise. \n \n Type 'D' to install the ion.d service on your ARM devise. \n \n \n Type 'Q' to quit. \n \n \n ")
 
 if choice == 'A' or choice == 'a':
-        print("\n Okay, installing QT wallet on your personal computer. \n")
+        print("\n Okay, installing ion QT wallet. \n")
         qt()
 elif choice == 'B' or choice == 'b':
-        print("\n Okay, installing QT wallet on your VPS. \n")
-        vps_qt()
+        print("\n Okay, installing the ion.d service. \n")
+        iond()
 elif choice == 'C' or choice == 'c':
-        print("\n Okay, installing QT wallet on your Raspberry Pi or Pine64. \n")
+        print("\n Okay, installing the ion QT wallet on your ARM device. \n")
         arm_qt()
 elif choice == 'D' or choice == 'd':
-        print("\n Okay, installing the ion.d service on your computer. \n")
-        iond()
-elif choice == 'E' or choice == 'e':
-        print("\n Okay, installing the ion.d service on your VPS. \n")
-        vps_iond()
-elif choice == 'F' or choice == 'f':
-        print("\n Okay, installing the ion.d servce on your Raspberry Pi or Pine64. \n")
+        print("\n Okay, installing the ion.d service on your ARM device. \n")
         arm_iond()
 elif choice == 'Q' or choice == 'q':
         print("\n Okay, goodbye. \n")
