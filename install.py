83c06a39f4a833be0769c9768e9d187de1ae7e13
sk00t3r <johnsone@roadrunner.com>
2017-02-27 23:12:23 -0500

    Update install.py

--- install.py
+++ install.py
@@ -8,6 +8,8 @@ import os
 distro = platform.linux_distribution()[0]
 version = platform.linux_distribution()[1]
 name =  platform.linux_distribution()[2]
+mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+mem = mem_bytes/(1024.**3)
 
 def qt():
         if version == "14.04" and ram > 4:
