9b0d2f6537bfcf7c9e9a6292d034ce4e4c0bad7d
sk00t3r <johnsone@roadrunner.com>
2017-03-18 10:51:09 -0400

    Update ubuntu14qt_lowram.sh

--- debian/qt/ubuntu14qt_lowram.sh
+++ debian/qt/ubuntu14qt_lowram.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 echo "#### Changing to home directory ####"
+echo " "
 cd ~
 clear
 echo "#### Installing Sudo ####"
+echo " "
 apt-get install sudo -y
 echo "#### Adding Required Repositories ####"
+echo " "
 sudo add-apt-repository ppa:silknetwork/silknetwork
 clear
 echo "#### Updating Ubuntu/Debian ####"
+echo " "
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get update -y
 clear
 echo "#### Creating Swap ####"
+echo " "
 sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -23,6 +28,7 @@ sudo swapon -s
 sudo echo "/swapfile swap sw 0 0" >> /etc/fstab
 clear
 echo "#### Installing Dependencies ####"
+echo " "
 sudo apt-get install autoconf -y
 sudo apt-get install autogen -y
 sudo apt-get install automake -y
@@ -65,14 +71,47 @@ sudo apt-get install libminiupnpc-dev -y
 sudo apt-get install zip -y
 clear
 echo "#### Cloning Repo ####"
+echo " "
 git clone https://github.com/ionomy/ion
 clear
 echo "#### Changing To The Ion-Core Directory ####"
+echo " "
 cd ion/
 clear
 echo "#### Making ####"
+echo " "
 sudo qmake
 sudo make
 clear
+cho "#### Chevking for existing iond install ####"
+echo " "
+if [ -n "$(ls -A ~/.ionomy/wallet.dat)" ] && [ -n "$(ls -A ~/.ionomy/ion.conf)" ]
+then
+  clear
+  echo "#### Backing up original ion wallet.dat & ion.conf ####"
+  echo " "
+  sudo cp -p -f -r ~/.ionomy/wallet.dat ~/.ionomy/wallet.dat.backup
+  sudo cp -p -f -r ~/.ionomy/ion.conf ~/.ionomy/ion.conf.backup
+elif [ -n "$(ls -A ~/.ion/wallet.dat)" ] && [ -n "$(ls -A ~/.ion/ion.conf)" ]
+  then
+  clear
+  echo "#### Backing up & moving old ion wallet.dat & ion.conf ####"
+  echo " "
+  sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
+  sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
+  sudo mv ~/.ion/wallet.dat ~/.ionomy/
+  sudo mv ~/.ion/ion.conf ~/.ionomy/
+else
+  clear
+  echo "#### Please set a username and password, the password should be long and random ####"
+  echo "#### Ctrl + X, Y, Enter to save file and exit ####"
+  echo " "
+  read -p "#### Press any key when you are ready to continue ####"
+  Echo " "
+  sudo wget https://raw.githubusercontent.com/sk00t3r/linux-ion/master/ion.conf
+  sudo nano ion.conf
+fi
+clear
 echo "#### Starting Ion-QT Wallet ####"
+echo " "
 sudo ./ionx-qt &
