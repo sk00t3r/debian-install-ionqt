0a71248e6cf73ca4ce8d6334c62e6f8d4404771c
sk00t3r <johnsone@roadrunner.com>
2017-04-11 23:24:24 -0400

    Update ubuntu17iond.sh

--- debian/iond/ubuntu17iond.sh
+++ debian/iond/ubuntu17iond.sh
@@ -91,7 +91,7 @@ else
   clear
   echo "#### No Existing Wallet Found, Installing IOND Core ####"
   echo " "
-  cd ion-test/src
+  cd ion-testnet/src
   sudo make -f makefile.unix
   sudo mv ~/ion-testnet/src/xiond ~/ion-testnet/src/iond
   sudo chmod 755 ~/ion-testnet/src/iond
