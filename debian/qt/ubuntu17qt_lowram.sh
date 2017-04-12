abedff89bba4c7ee9e29203d7aa871f80afcf184
sk00t3r <johnsone@roadrunner.com>
2017-04-12 14:09:26 -0400

    Update ubuntu17qt_lowram.sh

--- debian/qt/ubuntu17qt_lowram.sh
+++ debian/qt/ubuntu17qt_lowram.sh
@@ -105,7 +105,8 @@ else
   Echo " "
   sudo wget https://raw.githubusercontent.com/sk00t3r/linux-ion/blob/testnet/ion.conf
   sudo nano ion.conf
-  sudo cp -p -f -r ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
+  #sudo cp -p -f -r ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
+  sudo mv ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
 fi
 clear
 echo "#### Starting Ion-QT Wallet ####"
