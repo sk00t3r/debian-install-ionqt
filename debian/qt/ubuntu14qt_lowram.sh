a54e97b34fabf615369278c625871e562fe4f53b
sk00t3r <johnsone@roadrunner.com>
2017-04-23 03:34:17 -0400

    Update ubuntu14qt_lowram.sh

--- debian/qt/ubuntu14qt_lowram.sh
+++ debian/qt/ubuntu14qt_lowram.sh
@@ -85,13 +85,13 @@ sudo make
 clear
 cho "#### Chevking for existing iond install ####"
 echo " "
-if [ -n "$(ls -A ~/.ionomy/testnet/wallet.dat)" ] && [ -n "$(ls -A ~/.ionomy/testnet/ion.conf)" ]
+if [ -n "$(ls -A ~/.ionomy/testnet/wallet.dat)" ] && [ -n "$(ls -A ~/.ionomy/ion.conf)" ]
 then
   clear
   echo "#### Backing up original ion wallet.dat & ion.conf ####"
   echo " "
   sudo cp -p -f -r ~/.ionomy/testnet/wallet.dat ~/.ionomy/testnet/wallet.dat.backup
-  sudo cp -p -f -r ~/.ionomy/testnet/ion.conf ~/.ionomy/testnet/ion.conf.backup
+  sudo cp -p -f -r ~/.ionomy/testnet/ion.conf ~/.ionomy/ion.conf.backup
 #elif [ -n "$(ls -A ~/.ion/wallet.dat)" ] && [ -n "$(ls -A ~/.ion/ion.conf)" ]
 #  then
 #  clear
@@ -103,6 +103,7 @@ then
 #  sudo mv ~/.ion/ion.conf ~/.ionomy/
 else
   clear
+  sudo mkdir ~./ionomy/
   echo "#### Please set a username and password, the password should be long and random ####"
   echo "#### Ctrl + X, Y, Enter to save file and exit ####"
   echo " "
@@ -110,11 +111,10 @@ else
   echo " "
   sudo wget https://raw.githubusercontent.com/sk00t3r/linux-ion/blob/testnet/ion.conf
   sudo nano ion.conf
-  sudo mkdir ~/.ionomy
   sudo cp -p -f -r ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
   sudo rm ~/ion-testnet/ion.con
 fi
 clear
 echo "#### Starting Ion-QT Wallet ####"
 echo " "
-sudo ./ionx-qt -testnet &
+sudo ./ionx-qt
