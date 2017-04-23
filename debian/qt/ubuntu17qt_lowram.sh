914fb1ac0a5d1d9ea0983813e8f1b85e32fa0ff5
sk00t3r <johnsone@roadrunner.com>
2017-04-23 02:27:58 -0400

    Update ubuntu17qt_lowram.sh

--- debian/qt/ubuntu17qt_lowram.sh
+++ debian/qt/ubuntu17qt_lowram.sh
@@ -80,33 +80,33 @@ sudo make
 clear
 echo "#### Chevking for existing iond install ####"
 echo " "
-if [ -n "$(ls -A ~/.ionomy/wallet.dat)" ] && [ -n "$(ls -A ~/.ionomy/ion.conf)" ]
+if [ -n "$(ls -A ~/.ionomy/testnet/wallet.dat)" ] && [ -n "$(ls -A ~/.ionomy/testnet/ion.conf)" ]
 then
   clear
   echo "#### Backing up original ion wallet.dat & ion.conf ####"
   echo " "
-  sudo cp -p -f -r ~/.ionomy/wallet.dat ~/.ionomy/wallet.dat.backup
-  sudo cp -p -f -r ~/.ionomy/ion.conf ~/.ionomy/ion.conf.backup
-elif [ -n "$(ls -A ~/.ion/wallet.dat)" ] && [ -n "$(ls -A ~/.ion/ion.conf)" ]
-  then
-  clear
-  echo "#### Backing up & moving old ion wallet.dat & ion.conf ####"
-  echo " "
-  sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
-  sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
-  sudo mv ~/.ion/wallet.dat ~/.ionomy/
-  sudo mv ~/.ion/ion.conf ~/.ionomy/
+  sudo cp -p -f -r ~/.ionomy/testnet/wallet.dat ~/.ionomy/testnet/wallet.dat.backup
+  sudo cp -p -f -r ~/.ionomy/testnet/ion.conf ~/.ionomy/testnet/ion.conf.backup
+#elif [ -n "$(ls -A ~/.ion/wallet.dat)" ] && [ -n "$(ls -A ~/.ion/ion.conf)" ]
+#  then
+#  clear
+#  echo "#### Backing up & moving old ion wallet.dat & ion.conf ####"
+#  echo " "
+#  sudo cp -p -f -r ~/.ion/wallet.dat ~/.ion/wallet.dat.backup
+#  sudo cp -p -f -r ~/.ion/ion.conf ~/.ion/ion.conf.backup
+#  sudo mv ~/.ion/wallet.dat ~/.ionomy/
+#  sudo mv ~/.ion/ion.conf ~/.ionomy/
 else
   clear
   echo "#### Please set a username and password, the password should be long and random ####"
   echo "#### Ctrl + X, Y, Enter to save file and exit ####"
   echo " "
   read -p "#### Press any key when you are ready to continue ####"
-  Echo " "
+  echo " "
   sudo wget https://raw.githubusercontent.com/sk00t3r/linux-ion/testnet/ion.conf
   sudo nano ion.conf
-  #sudo cp -p -f -r ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
-  sudo mv ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
+  sudo cp -p -f -r ~/ion-testnet/ion.conf ~/.ionomy/ion.conf
+  sudo rm ~/ion-testnet/ion.conf
 fi
 clear
 echo "#### Starting Ion-QT Wallet ####"
