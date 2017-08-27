cc6fd01596cb68c3949ff5424b135dfe7ae39aa3
sk00t3r <johnsone@roadrunner.com>
2017-08-26 21:51:23 -0400

    Update ubuntu17iond_lowram.sh

--- debian/iond/ubuntu17iond_lowram.sh
+++ debian/iond/ubuntu17iond_lowram.sh
@@ -18,7 +18,7 @@ echo " "
 cd ~/
 clear
 echo "#### Adding Bicoin PPA ####"
-#sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo add-apt-repository ppa:ionomy/ioncoin
 clear
 echo "#### Updating Ubuntu/Debian ####"
