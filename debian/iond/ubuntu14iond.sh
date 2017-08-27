022e70ec52f827bce82dd6d16bbc1abd1cb1bcbb
sk00t3r <johnsone@roadrunner.com>
2017-08-27 03:09:25 -0400

    Update ubuntu14iond.sh

--- debian/iond/ubuntu14iond.sh
+++ debian/iond/ubuntu14iond.sh
@@ -18,6 +18,7 @@ echo " "
 cd ~/
 clear
 echo "#### Adding Bitcoin PPA ####"
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:bitcoin/bitcoin
 #sudo add-apt-repository ppa:ionomy/ioncoin
 clear
